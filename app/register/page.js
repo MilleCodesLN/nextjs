322dd1037224170d2ab6798c27cc978d0f98c76c
MilleCodesLN <48634455+MilleCodesLN@users.noreply.github.com>
2025-04-29 16:34:06 +0200

    Update page.js

--- app/register/page.js
+++ app/register/page.js
@@ -1,2 +1,23 @@
 'use client'
 import {useState} from 'react';
+
+export default function RegisterPage(){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+const handleSubmit = async (e)=>{
+  e.preventDefault();
+  const res = await fetch('api/register', {
+    method: 'POST',
+    body: JSON.stringify({email, password}),
+  });
+  if(res.ok){
+    alert('Compte crée, vous pouvez vous connecter.');
+  } else {
+    alert('Erreur lors de l'inscription.');
+  }
+};
+
+return (
+  
+  
