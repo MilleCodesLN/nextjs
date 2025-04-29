57c79bcc1e01ab6a659bb0550c3e7e2bcb2c6c9c
MilleCodesLN <48634455+MilleCodesLN@users.noreply.github.com>
2025-04-29 16:35:35 +0200

    Update page.js

--- app/register/page.js
+++ app/register/page.js
@@ -5,19 +5,24 @@ export default function RegisterPage(){
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-const handleSubmit = async (e)=>{
-  e.preventDefault();
-  const res = await fetch('api/register', {
-    method: 'POST',
-    body: JSON.stringify({email, password}),
-  });
-  if(res.ok){
-    alert('Compte crée, vous pouvez vous connecter.');
-  } else {
-    alert('Erreur lors de l'inscription.');
-  }
-};
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    const res = await fetch('api/register', {
+      method: 'POST',
+      body: JSON.stringify({email, password}),
+    });
+    if(res.ok){
+      alert('Compte crée, vous pouvez vous connecter.');
+    } else {
+      alert('Erreur lors de l'inscription.');
+    }
+  };
 
-return (
-  
-  
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" placeholder="Email" value={email} onChange{(e)=> setEmail(e.target.value)} />
+      <input type="password" placeholder="Mot de passe" value={password} onChange{(e)=> setPassword(e.target.value)} />
+      <button type="submit">Créer un compte</button>
+    </form>
+  );
+}
